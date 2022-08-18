2ebdcba2c3af8a747c42d58a5ef2c1290d742aca
Developer Hash <tech247gb@gmail.com>
2022-08-18 11:58:47 +0530

    Database seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$10$vsglXvEbRt8bEV78gdBSgu6Bxi/ZYIH64lLULE8WX40QXMMCukjsO',
+            'phnumber' => '9645584806',
+            'role' => '0'
+        ]);
     }
 }
